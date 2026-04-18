ac7a1e8e3c02acc35696576e12f39ad6e570dc99
neetiv <67287846+neetiv@users.noreply.github.com>
2026-04-18 13:03:19 -0700

    Initial Commit

---
+++ Recipository/RecipositoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipositoryApp.swift
+//  Recipository
+//
+//  Created by Neeti Vaidya on 4/18/26.
+//
+
+import SwiftUI
+
+@main
+struct RecipositoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
