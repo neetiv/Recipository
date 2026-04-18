1f3aaebd19df8969e2bf7c6b27aa8a3a7f51cedb
neetiv <67287846+neetiv@users.noreply.github.com>
2026-04-18 15:50:28 -0700

    Rename TimerManager to TimerManager.swift - fix from text to code

--- Recipository/ContentView.swift
+++ Recipository/ContentView.swift
@@ -1,40 +1,54 @@
 //
-//  ContentView.swift
-//  Recipository
+//  ContentView.swift
+//  Recipository
 //
 
 import SwiftUI
 
 struct ContentView: View {
-    @State private var currentPage: Page = .voiceCommands
-    @State private var selectedMeal: Meal? = nil
+    @State private var currentPage: Page = .voiceCommands
+    @State private var selectedMeal: Meal?
 
-    enum Page {
-        case voiceCommands, recipeList, recipe
-    }
+    enum Page {
+        case voiceCommands, recipeList, ingredientsAndEquipment, recipe
+    }
 
-    var body: some View {
-        switch currentPage {
-        case .voiceCommands:
-            VoiceCommandsView(onDismiss: { currentPage = .recipeList })
+    var body: some View {
+        switch currentPage {
+        case .voiceCommands:
+            VoiceCommandsView(onDismiss: { currentPage = .recipeList })
 
-        case .recipeList:
-            RecipeListView { meal in
-                selectedMeal = meal
-                currentPage = .recipe
-            }
+        case .recipeList:
+            RecipeListView { meal in
+                selectedMeal = meal
+                currentPage = .ingredientsAndEquipment
+            }
 
-        case .recipe:
-            if let meal = selectedMeal {
-                RecipeView(meal: meal, onBack: {
-                    selectedMeal = nil
-                    currentPage = .recipeList
-                })
-            }
-        }
-    }
+        case .ingredientsAndEquipment:
+            if let meal = selectedMeal {
+                IngredientsAndEquipmentView(
+                    meal: meal,
+                    onBack: {
+                        selectedMeal = nil
+                        currentPage = .recipeList
+                    },
+                    onStartRecipe: {
+                        currentPage = .recipe
+                    }
+                )
+            }
+
+        case .recipe:
+            if let meal = selectedMeal {
+                RecipeView(meal: meal, onBack: {
+                    currentPage = .ingredientsAndEquipment
+                })
+            }
+        }
+    }
 }
 
 #Preview(windowStyle: .automatic) {
-    ContentView()
+    ContentView()
 }
+
